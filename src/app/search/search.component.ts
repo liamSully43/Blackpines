eaf3315173520fe020f96bb78ed4b2af322c46db
liamSully43 <60324016+liamSully43@users.noreply.github.com>
2020-10-27 18:46:03 +0000

    Cleaned up user & post results
     - Added additional relevent information to the results for both users & posts
     - Added a verification badge for verified twitter accounts
     - Added a following badge for accounts the user is following

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -57,12 +57,47 @@ export class SearchComponent implements OnInit {
     const type = this.searchType;
     this.http.post("api/search", { headers, searchTerm, type }, {responseType: "json"}).subscribe(((result: any) => {
       if(result.success) {
-        if(this.searchType === "Users") {
+        if(this.searchType === "Users") { // if user's was searched for
+          let users = result.results;
+          for(let user of users) {
+            let followers = user.followers_count;
+            let following = user.friends_count;
+            // round down num of followers
+            if(followers >= 1000 && followers <= 999999) { // if the user has between 1000 & 999,999 followers then round down to the nearest 1000
+              followers = Math.floor(followers/1000)
+              followers += "K";
+              user.followers_count = followers;
+            }
+            else if(followers >= 1000000) { // if the user has more than 1,000,000 followers then round down to the nearest million
+              followers = Math.floor(followers/1000000);
+              followers += "M";
+              user.followers_count = followers;
+            }
+
+            // round down num of following
+            if(following >= 1000 && following <= 999999) {
+              following = Math.floor(following/1000)
+              following += "K";
+              user.friends_count = following;
+            }
+            else if(following >= 1000000) {
+              following = Math.floor(following/1000000);
+              following += "M";
+              user.friends_count = following;
+            }
+          }
           this.twitterResults = result.results;
           console.log(this.twitterResults);
         }
-        else {
-          this.twitterResults = result.results.statuses;
+        else { // if posts were searched for
+          let posts = result.results.statuses;
+          for(let post of posts) {
+            let time = post.created_at;
+            let date = post.created_at;
+            post.time = time.substr(11, 5);
+            post.date = date.substr(4, 6);
+          }
+          this.twitterResults = posts;
           console.log(this.twitterResults);
         }
       }
